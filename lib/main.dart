b7e3f66f8f08e1451a88cd10b7500275f54470a9
Fomagran <fomagran6@naver.com>
2021-04-18 17:09:08 +0900

    버튼 7개 추가해서 소리 다 다르게 나게 하기

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-              child: FlatButton(
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note1.wav');
-                  },
-                  child: Text("Click Me"))),
-        ),
+        home: Scaffold(
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          FlatButton(
+            color: Colors.red,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.orange,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note2.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.yellow,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note3.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.green,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note4.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.blue,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note5.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.indigo,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note6.wav');
+            },
+          ),
+          FlatButton(
+            color: Colors.purple,
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note7.wav');
+            },
+          ),
+        ]),
       ),
-    );
+    ));
   }
 }
