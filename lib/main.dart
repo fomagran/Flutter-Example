06e9921139bade3666c1b0365c94c18f6fd1cb32
Fomagran <fomagran6@naver.com>
2021-04-18 13:38:38 +0900

    이미지에 패딩넣기

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,22 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        //이미지 첨부 이렇게 줄여서 쓸수도 있음.
-        Expanded(child: Image.asset("images/dice1.png")),
-        Expanded(child: Image.asset("images/dice1.png")),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          //이미지 첨부 이렇게 줄여서 쓸수도 있음.
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("images/dice1.png"),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("images/dice1.png"),
+          )),
+        ],
+      ),
     );
   }
 }
