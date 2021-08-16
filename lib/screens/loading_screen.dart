2562485462aaec6a29a7fc281cbb11f1ea0167b1
Fomagran <fomagran6@naver.com>
2021-08-16 14:04:13 +0900

    location_screen 화면 추가

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
-//as http를 쓰면 앞에 이름을 http로 대체할 수 있음.
-import 'package:http/http.dart' as http;
+import 'package:weather/services/networking.dart';
+import 'location_screen.dart';
+
+const apiKey = '1a5b183fc3a82b25cc1095a55e549a0b';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,34 +11,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-  }
+    longitude = location.longitude;
+    latitude = location.latitude;
 
-  void getData() async {
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=1a5b183fc3a82b25cc1095a55e549a0b'));
-    if (response.statusCode == 200) {
-      String data = response.body;
-      var lon = jsonDecode(data)['coord']['lon'];
-      var city = jsonDecode(data)['name'];
-      print(lon);
-      print(city);
-    } else {
-      print(response.statusCode);
-    }
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherDat = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(
         child: ElevatedButton(
