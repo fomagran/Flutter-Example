e8f8bd6e3a4d314c71b77898b5aa801b223c2022
Fomagran <fomagran6@naver.com>
2021-08-16 12:38:02 +0900

    json 데이터 디코딩해서 값 출력하기

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
 //as http를 쓰면 앞에 이름을 http로 대체할 수 있음.
@@ -25,6 +27,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=1a5b183fc3a82b25cc1095a55e549a0b'));
     if (response.statusCode == 200) {
       String data = response.body;
+      var lon = jsonDecode(data)['coord']['lon'];
+      var city = jsonDecode(data)['name'];
+      print(lon);
+      print(city);
     } else {
       print(response.statusCode);
     }
